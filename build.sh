94bf25030dcff96e1437b181d6b2632a2ba01ab6
Toomore Chiang <toomore0929@gmail.com>
2023-04-06 07:23:20 +0000

    Change manually repo
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 docker build -t element-web:demo \
     --build-arg USE_CUSTOM_SDKS=true \
-    --build-arg REACT_SDK_REPO="https://github.com/matrix-org/matrix-react-sdk.git" \
-    --build-arg REACT_SDK_BRANCH="develop" \
+    --build-arg REACT_SDK_REPO="https://github.com/toomore/matrix-org.matrix-react-sdk.git" \
+    --build-arg REACT_SDK_BRANCH="manually_i18n" \
     --build-arg JS_SDK_REPO="https://github.com/matrix-org/matrix-js-sdk.git" \
     --build-arg JS_SDK_BRANCH="develop" \
     .
